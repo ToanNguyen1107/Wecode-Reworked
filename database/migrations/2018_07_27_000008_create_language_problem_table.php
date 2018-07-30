025acbd71b53bfbc55a039ee32d06f44a9ccfbf0
chau259 <thanhnguyenfg@gmail.com>
2018-07-30 13:38:13 +0700

    push migrate

--- database/migrations/2018_07_27_000008_create_language_problem_table.php
+++ database/migrations/2018_07_27_000008_create_language_problem_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLanguageProblemsTable extends Migration
+class CreateLanguageProblemTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateLanguageProblemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('language_problems', function (Blueprint $table) {
+        Schema::create('language_problem', function (Blueprint $table) {
             $table->increments('id');           
             $table->unsignedInteger('memory_limit')->nullable();
             $table->unsignedInteger('time_limit')->nullable();
             $table->timestamps();
         });
 
-        Schema::table('language_problems', function (Blueprint $table) {
+        Schema::table('language_problem', function (Blueprint $table) {
             $table->unsignedInteger('problem_id')->nullable();
             $table->foreign('problem_id')->references('id')->on('problems');
             $table->unsignedInteger('language_id')->nullable();
